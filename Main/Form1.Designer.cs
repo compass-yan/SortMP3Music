9a6cb8075a1f92a1936e38892737e9bb6674ae18
sesame <sesamefly@163.com>
2018-02-20 22:47:52 +0800

    ini

---
+++ Main/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Main
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.cmbDisks = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblfail = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSuc = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始排序";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbDisks
+            // 
+            this.cmbDisks.FormattingEnabled = true;
+            this.cmbDisks.Location = new System.Drawing.Point(257, 55);
+            this.cmbDisks.Name = "cmbDisks";
+            this.cmbDisks.Size = new System.Drawing.Size(148, 26);
+            this.cmbDisks.TabIndex = 3;
+            this.cmbDisks.SelectedIndexChanged += new System.EventHandler(this.cmbDisks_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(440, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "总条数：";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(526, 158);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(17, 18);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "失败条数：";
+            // 
+            // lblfail
+            // 
+            this.lblfail.AutoSize = true;
+            this.lblfail.Location = new System.Drawing.Point(342, 158);
+            this.lblfail.Name = "lblfail";
+            this.lblfail.Size = new System.Drawing.Size(17, 18);
+            this.lblfail.TabIndex = 7;
+            this.lblfail.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "成功条数：";
+            // 
+            // lblSuc
+            // 
+            this.lblSuc.AutoSize = true;
+            this.lblSuc.Location = new System.Drawing.Point(162, 158);
+            this.lblSuc.Name = "lblSuc";
+            this.lblSuc.Size = new System.Drawing.Size(17, 18);
+            this.lblSuc.TabIndex = 8;
+            this.lblSuc.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "选择要排序的磁盘：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 259);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblfail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbDisks);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "可移动磁盘MP3排序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.ComboBox cmbDisks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblfail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSuc;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Main/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class Form1 : Form
+    {
+        string  path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var files = Directory.GetFiles(path, "*.mp3");
+            Array.Sort(files);
+            var errorCount = 0;
+            var sucCount = 0;
+            var index = 0;
+            foreach (var item in files)
+            {
+                ++index;
+                var ctDate = DateTime.Now.AddSeconds(index);
+                var file = new FileInfo(item);
+                if (file!=null)
+                {
+                    file.CreationTime = ctDate;
+                    sucCount++;
+                    lblSuc.Text = sucCount.ToString();
+                }
+                else
+                {
+                    errorCount++;
+                    lblfail.Text = errorCount.ToString();
+
+                }
+            }
+            MessageBox.Show("已完成！");
+
+        }
+
+        private void cmbDisks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            path = cmbDisks.SelectedItem.ToString() ;
+            var files = Directory.GetFiles(path, "*.mp3");
+
+            lblTotal.Text = files.Length.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var divices=DriveInfo.GetDrives();
+            List<string> divicesRemovalbe= new List<string> { };
+            foreach (var item in divices)
+            {
+                if (item.DriveType == DriveType.Removable)
+                {
+                    divicesRemovalbe.Add(item.Name);
+                }
+            }
+            cmbDisks.DataSource = divicesRemovalbe;
+        }
+    }
+}
